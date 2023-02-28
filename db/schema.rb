3e78d60e5e7d63e7126efdc362b6acd596c35b25
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-02-28 13:56:16 +0000

    [add]userとbookのアソシエーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_27_141018) do
+ActiveRecord::Schema.define(version: 2023_02_28_134443) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,10 +42,10 @@ ActiveRecord::Schema.define(version: 2023_02_27_141018) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "body"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "opinion"
   end
 
   create_table "users", force: :cascade do |t|
