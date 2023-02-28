05206949eb3e1cc746f9121a0dc1d5d71fb43e8a
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-02-28 16:18:43 +0000

    [add]booksコントローラへcreateアクションを追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,22 @@
 class BooksController < ApplicationController
-  def new
+  def index
+    @book = Book.new
   end
 
-  def index
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def show
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
