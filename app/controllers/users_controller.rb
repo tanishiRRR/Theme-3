89722f8a7e9dc05989e3746b0d073b3c3f5c56b2
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-03-06 15:09:51 +0000

    [add]homesのviewとsignup,loginのレイアウト編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
+    @user.update(book_params)
     redirect_to user_path(@user.id)
   end
 
