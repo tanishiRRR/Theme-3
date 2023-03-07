0fe5c4f4fc89235ced94b04d2dc44182ad6b9557
RyoheiOtani <124274331+tanishiRRR@users.noreply.github.com>
2023-03-07 15:59:51 +0000

    [add]book.rbのバリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
